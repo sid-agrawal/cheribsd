451c5fdf81e145e7b2b9ec459b26ab1fbcbd2a04
Shaurya Patel <spatel27@student.ubc.ca>
2023-01-04 14:33:24 -0800

    Measuring best case performance

--- sys/vm/vm_fault.c
+++ sys/vm/vm_fault.c
@@ -1396,7 +1396,7 @@ vm_fault_getpages(struct faultstate *fs, int *behindp, int *aheadp)
 		int count = 0;
 		// vm_map_lock(fs->map);
 		//printf("mvu is %lu, mve is %lu\n", cheri_getaddress(mvu), mve);	
-		for(; cheri_getaddress(mvu) < mve && count < 4; mvu++) {
+		for(; cheri_getaddress(mvu) < mve && count < 1; mvu++) {
 			if(cheri_gettag(*mvu)) {
 				vm_offset_t vaddr = cheri_getaddress(mvu);
 				if (trunc_page(vaddr) == 

37fc9da7db9c7feb2c0237d7e6a2544d6cba95c3
Sid Agrawal <sid@sid-agrawal.ca>
2023-01-27 14:11:26 -0800

    Add a inorder tree traversal BM

--- cp_benchmarks/pointers/tree2.c
+++ cp_benchmarks/pointers/tree2.c
@@ -1,10 +1,15 @@
 // From:
 // https://www.geeksforgeeks.org/construct-complete-binary-tree-given-array/
-// CPP program to construct binary
-// tree from given array in level
-// order fashion Tree Node
+// CPP program to construct binary of a given depth 
+// Traverse: in order
 #include <bits/stdc++.h>
+#include <chrono>
 using namespace std;
+
+int node_count = 0;
+int cycle_per_node;
+
+#define PAGE_SIZE 4096
 
 /* A binary tree node has data_int, and data_char
 pointer to left child and a
@@ -12,7 +17,7 @@ pointer to right child */
 struct Node
 {
 	int data_int;
-	char data_char[4096 - sizeof(Node *) * 2 - sizeof(int)];
+	char data_char[PAGE_SIZE - sizeof(Node *) * 2 - sizeof(int)];
 	Node* left, * right;
 };
 
@@ -21,26 +26,32 @@ new node */
 Node* newNode(int data_int)
 {
 	Node* node = (Node*)malloc(sizeof(Node));
+        if (node == NULL){
+                cout <<  "malloc failed\n";
+                exit(1);
+        }
+        //cout << "Allocating: " << sizeof(Node) << "Bytes";
 	node->data_int = data_int;
 	node->left = node->right = NULL;
 	return (node);
 }
 
 // Function to insert nodes in level order
-Node* insertLevelOrder(int arr[], int i, int n)
+Node* insertLevelOrder(int i, int n)
 {
 	Node *root = nullptr;
 	// Base case for recursion
 	if (i < n)
 	{
-		root = newNode(arr[i]);
+		root = newNode(node_count);
+                node_count++;
 		
 		// insert left child
-		root->left = insertLevelOrder(arr,
+		root->left = insertLevelOrder(
 				2 * i + 1, n);
 
 		// insert right child
-		root->right = insertLevelOrder(arr,
+		root->right = insertLevelOrder(
 				2 * i + 2, n);
 	}
 	return root;
@@ -49,20 +60,60 @@ Node* insertLevelOrder(int arr[], int i, int n)
 // Function to print tree nodes in
 // InOrder fashion
 void inOrder(Node* root)
-{
+{ 
+        /* Volatile so that it doesn't get optimized out */
+        volatile int busy_work;
 	if (root != NULL)
 	{
+                for (int i = 0; i < cycle_per_node; i++) {
+                        busy_work++;
+                }
 		inOrder(root->left);
-		cout << root->data_int <<" ";
+		//cout << root->data_int <<" ";
 		inOrder(root->right);
 	}
 }
 
 // Driver program to test above function
-int main()
+int main(int argc,char* argv[])
 {
-	int arr[] = { 1, 2, 3, 4, 5, 6, 6, 6, 6 };
-	int n = sizeof(arr)/sizeof(arr[0]);
-	Node* root = insertLevelOrder(arr, 0, n);
+        if (argc != 3){
+                cout << "Usage: tree <depth> <cyclesPerNode>\n";
+                exit(1);
+        }
+
+        /* Get Depth */
+        int depth = atoi(argv[1]); 
+
+        /* Get cycle count */
+        cycle_per_node = atoi(argv[2]);
+
+        /* Calculate Total Number of Nodes*/
+	long n = 1ULL << (depth-1);
+        
+        /* Print Summary */
+        cout << "Depth is: " << depth << "\n";  
+        cout << "Cycle Per Node is: " << cycle_per_node << "\n";  
+        cout << "Count is: " << n << "\n";  
+        cout << "Approx Memory consumption: " << PAGE_SIZE*n/1024/1024 << " MB\n";  
+        
+        std::chrono::steady_clock::time_point begin,end;
+        begin = std::chrono::steady_clock::now();
+
+
+        cout << "START of Construction\n";
+        begin = std::chrono::steady_clock::now();
+	Node* root = insertLevelOrder(0, n);
+        end = std::chrono::steady_clock::now();
+        cout << "END   of Construction. Duration" << 
+         std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() 
+         << "[s]" << std::endl;
+
+        cout << "START of Traversal\n";
+        begin = std::chrono::steady_clock::now();
 	inOrder(root);
+        end = std::chrono::steady_clock::now();
+        cout << "END   of Traversal. Duration" << 
+         std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() 
+         << "[s]" << std::endl;
 }

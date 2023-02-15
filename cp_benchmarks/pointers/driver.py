690ac6bb004bc19f0ba81edad44a06054d5147bd
Sid Agrawal <sid@sid-agrawal.ca>
2023-02-15 18:10:55 +0000

    sid: A basic tree version is there

--- cp_benchmarks/pointers/driver.py
+++ cp_benchmarks/pointers/driver.py
@@ -3,31 +3,51 @@
 import argparse
 import sys
 import subprocess
+from python_sysctl import sysctlRead, sysctlWrite
 
+
+important_stat = ( 
+        b"vm.v_prefetches", 
+        b"vm.v_majorfault",
+        b"vm.v_majorfault_latency",
+        b"vm.v_async_io_latency",
+        b"vm.v_prefetches",
+        b"vm.v_prefetch_latency",
+        b"vm.v_blocked_softfault",
+        b"vm.v_prefetches"
+        )
+
+def debugPrint(args):
+    print()
 
 def runCommand( cmd ):
     result = subprocess.run(cmd, capture_output=True)
     print(repr(result.stdout))
   
 def enableCP():
-    print("enabling the magical CHERI picking")
+    newValue = sysctlWrite(b"vm.v_cheri_prefetch", 1)
+    assert(newValue == 1)
 
 def disableCP():
-    print("enabling the magical CHERI picking")
+    newValue = sysctlWrite(b"vm.v_cheri_prefetch", 0)
+    assert(newValue == 0)
 
 def resetStats():
-    print("Resetting Stats")
+    for x in important_stat:
+        sysctlWrite(x, 0)
 
 def collectStats():
-    print("Collecting Stats")
+    stats = {}
+    for x in important_stat:
+        stats[ x ] = sysctlRead(x)
+    return stats
 
 def main(argv):
-
-
     parser = argparse.ArgumentParser()
+    parser.add_argument('-d','--debug', help='Debug Enabled')
 
     # create sub-parser
-    sub_parsers = parser.add_subparsers(help='sub-command help')
+    sub_parsers = parser.add_subparsers(help='sub-command help', dest='subcommand')
 
     # create the parser for the "ll" sub-command
     parser_ll = sub_parsers.add_parser('ll', help='Linked list pointer benchmark')
@@ -35,6 +55,7 @@ def main(argv):
     parser_ll.add_argument('--start-delay', type=int, help='Delay for each node visited as loop count')
     parser_ll.add_argument('--step-delay', type=int, help='Delay Increments')
     parser_ll.add_argument('--end-delay', type=int, help='End Delay')
+    parser_ll.set_defaults(func=handle_ll)
 
     # create the parser for the "tree" sub-command
     parser_tree = sub_parsers.add_parser('tree', help='Tree pointer benchmark')
@@ -42,16 +63,28 @@ def main(argv):
     parser_tree.add_argument('--step-delay', type=int, help='Delay Increments')
     parser_tree.add_argument('--end-delay', type=int, help='End Delay')
     parser_tree.add_argument('--depth', type=int, help='Depth of Tree')
+    parser_tree.set_defaults(func=handle_tree)
     
     args = parser.parse_args()
-    print("args=%s" % args)
+    args.func(args)
 
-    print("args.name=%s" % args.test)
-    cmd = [ args.test, "-la" ]
-    #runCommand(cmd)
+
+def handle_ll(subparser):
+    print("In ll", subparser)
+
+def handle_tree(args):
+    print("Enabling CP")
+    enableCP()
 
     for x in range(args.start_delay, args.end_delay, args.step_delay):
-        print(x)
+        runCommand([ "./tree", str(args.depth), str(x)])
+
+
+    print("Disabling CP")
+    disableCP()
+        
+    for x in range(args.start_delay, args.end_delay, args.step_delay):
+        runCommand([ "./tree", str(args.depth), str(x)])
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- cp_benchmarks/pointers/python_sysctl.py
+++ cp_benchmarks/pointers/python_sysctl.py
@@ -3,38 +3,38 @@ from ctypes.util import find_library
 libc = CDLL(find_library("c"))
 
 def sysctlRead(name):
-    size = c_uint(0)
+    
     # Find out how big our buffer will be
-    libc.sysctlbyname(name, None, byref(size), None, 0)
-    # Make the buffer
-    value = c_uint64(0)
-    # Re-run, but provide the buffer
-    libc.sysctlbyname(name, byref(value), byref(size), None, 0)
-    return value
-
-def sysctlWrite(name, value):
     size = c_uint(0)
-    # Find out how big our buffer will be
     rv = libc.sysctlbyname(name, None, byref(size), None, 0)
     assert(rv == 0)
+    
     # Make the buffer
+    value = c_uint64(0)
+    
+    # Re-run, but provide the buffer
+    rv = libc.sysctlbyname(name, byref(value), byref(size), None, 0)
+    assert(rv == 0)
+    
+    return value.value
+
+def sysctlWrite(name, value):
+    
+    # Find out how big our buffer will be
+    size = c_uint(0)
+    rv = libc.sysctlbyname(name, None, byref(size), None, 0)
+    assert(rv == 0)
+    
+    # Make the buffer for old and new value
     oldValue = c_uint64(0)
     newValue = c_uint64(value)
+    
     # Re-run, but provide the buffer
     rv = libc.sysctlbyname(name, 
             byref(oldValue), byref(size), 
             byref(newValue), size)
     assert(rv == 0)
-    return value
+    assert(value == newValue.value)
 
-name = b"vm.v_prefetches"
-print("Reading ", name)
-value = sysctlRead(name)
-print("\t", value)
 
-print("Writing ", name)
-sysctlWrite(name, 0)
-
-print("Reading ", name)
-value = sysctlRead(name)
-print("\t", value)
+    return newValue.value
